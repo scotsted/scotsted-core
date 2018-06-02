510b3f647c06d4e70cee43fd8f0cd9fdfd934d3e
David Scott <david@scotsted.uk>
2018-06-02 17:08:07 +0100

    Use status code pages and welcome page.

---
+++ ScotstedWebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScotstedWebApplication.Controllers
+{
+    public class ValuesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var statusCodePagesFeature = HttpContext.Features.Get<IStatusCodePagesFeature>();
+            if (statusCodePagesFeature != null)
+            {
+                statusCodePagesFeature.Enabled = false;
+            }
+            return StatusCode(500);
+        }
+    }
+}
